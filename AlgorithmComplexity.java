efb3717e935ea9686716a08f7ac9d8e8504280aa
minhtuan <37221796+tuanpmt38@users.noreply.github.com>
2018-04-22 22:10:00 +0700

    pf-java-search-algorithm-complexity

---
+++ AlgorithmComplexity.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class AlgorithmComplexity {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String inputString = scanner.nextLine();
+        /*codes below here */
+        int[] frequentChar = new int[255];
+        for (int i = 0; i < inputString.length(); i++) {
+            /* Tìm ví trí tương ứng của ký tự trong bảng ASCII */
+            int ascii = (int) inputString.charAt(i);
+            /* Tăng giá trị tần suất */
+            frequentChar[ascii] += 1;
+        }
+
+        int max = 0;
+        char character = (char) 255; /* empty character */
+        for (int j = 0; j < 255; j++) {
+            if (frequentChar[j] > max) {
+                max = frequentChar[j];
+            }
+        }
+        System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
+    }
+}
